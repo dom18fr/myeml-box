c0e1861bb36f36dc6331355e2f7be2b52d82df98
Dominique Fabre <dfabre@clever-age.com>
2017-07-09 13:50:50 +0200

    Update generic namespace in Data & Value classes annotation

--- apps/www/cdm/src/MyEML/EAVModelBundle/Entity/Value.php
+++ apps/www/cdm/src/MyEML/EAVModelBundle/Entity/Value.php
@@ -6,7 +6,7 @@ use CleverAge\EAVManager\EAVModelBundle\Entity\AbstractValue;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Table(name="client_value", indexes={
+ * @ORM\Table(name="myeml_value", indexes={
  *     @ORM\Index(name="attribute", columns={"attribute_code"}),
  *     @ORM\Index(name="string_search", columns={"attribute_code", "string_value"}),
  *     @ORM\Index(name="int_search", columns={"attribute_code", "integer_value"}),
