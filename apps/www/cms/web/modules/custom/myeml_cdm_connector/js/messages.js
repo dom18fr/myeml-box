b216b96dff67777d6c660108ac2af9b480707d0d
Julien Kruppa <jkruppa@clever-age.com>
2017-07-11 18:08:55 +0200

    Use postMessages on view Node-Select

--- apps/www/cms/web/modules/custom/myeml_cdm_connector/js/messages.js
+++ apps/www/cms/web/modules/custom/myeml_cdm_connector/js/messages.js
@@ -1,6 +1,38 @@
 (function ($, Drupal, drupalSettings) {
+
+  function sendPostMessageJson(json) {
+    console.log('SEND ' + json);
+    parent.postMessage(json, drupalSettings.myeml_cdm_connector.target);
+  }
+
+  function sendPostMessage(action, entity, id) {
+    var json = JSON.stringify([action, entity, id]);
+    sendPostMessageJson(json);
+  }
+
   // Send message if available.
   if (drupalSettings.myeml_cdm_connector.messages) {
-    parent.postMessage(drupalSettings.myeml_cdm_connector.messages, drupalSettings.myeml_cdm_connector.target);
+    //parent.postMessage(drupalSettings.myeml_cdm_connector.messages, drupalSettings.myeml_cdm_connector.target);
+    sendPostMessageJson(drupalSettings.myeml_cdm_connector.messages);
   }
+
+  $(function() {
+    $('.view-id-node_select td.views-field-nid').each(function () {
+      var $this = $(this);
+      var nid = parseInt($this.text());
+      $this.html('<input type="checkbox" value="' + nid + '" class="cdm-connector-nid" />');
+    });
+
+    $('.view-id-node_select .view-content').append('<input type="button" value="Select" class="button cdm-connector-select" />');
+
+    $('.cdm-connector-select').on('click', function (event) {
+      var nids = $('.cdm-connector-nid:checked').map(function () {
+        return this.value;
+      });
+      if (nids.length) {
+        sendPostMessage('select', 'node', $.makeArray(nids).join(','));
+        $('.cdm-connector-nid:checked').prop('checked', false);
+      }
+    });
+  });
 })(jQuery, Drupal, drupalSettings);
