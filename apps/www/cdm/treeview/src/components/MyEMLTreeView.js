84bc3887b048c976731a769f68df4d599e8298bd
Dominique Fabre <dfabre@clever-age.com>
2017-07-11 15:45:53 +0200

    Setup a basic treeview react app

---
+++ apps/www/cdm/treeview/src/components/MyEMLTreeView.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class MyEMLTreeView extends Component {
+  render () {
+    return (
+      <div>
+        <h1>My EMLyon CDM Tree view !</h1>
+      </div>
+    )
+  }
+}
---
+++ apps/www/cdm/treeview/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyEMLTreeView from "./components/MyEMLTreeView";
+
+ReactDOM.render(
+  <MyEMLTreeView />,
+  document.getElementById('myeml-treeview')
+);
